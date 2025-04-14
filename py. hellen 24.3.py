9ced22f2857f98fcf96ec8b01a5e072fca65800b
Hellen <hellenmariar20@gmail.com>
2025-04-14 14:09:17 -0300

    Adiciona commit inicial.

---
+++ py. hellen 24.3.py
@@ -0,0 +1,29 @@
+for i in range (11):
+    print(i)
+
+for i in range (5):
+    print("olá mundo")
+
+for i in range (1,10,2):
+    print(i)
+
+for i in range (10, 0, -1):
+    print(i)
+
+for i in range (100,4,-25):
+    print(i)
+)
+for i in range (0, 17, 4):
+    print(i)
+
+for i in range (5,0,-2):
+    print(i)
+
+for contador in range(1, 10, +1):
+    print(contador)
+
+n = 10
+soma = 0
+for i in range (1, n, + 1):
+    soma += i
+print (f"A soma dos primeiros {n} numeros naturais é: {soma}")
---
+++ py2 hellen 24.03.py
@@ -0,0 +1,41 @@
+#for i in range (1, 11):
+ #   print(i)
+
+#for i in range (2, 21, 2):
+ #    print(i)
+
+#for i in range (10, 0, -1):
+#    print(i)
+
+#n = 10
+#soma = 0
+#for i in range (1,n + 1):
+    #soma += i
+#print (f"a soma dos primeiros {n} numeros naturais é:  {soma})
+
+#for i in range (0, 10 + 1):
+    #print(f'5X{i} = {5*i}')
+
+#palavra = "hello word"
+#for i in range (len(palavra)):
+    #print(f"caracteristicas no indice {i}: {palavra[i]}")
+
+#frutas = ["maçã","banana", "laranja"]
+#for i in range(len(frutas)):
+   # print(f"Frutas no índice {i}: {frutas[i]}")
+
+#frutas = ["cereja", "manga", "mamão", "morango"]
+#for indice, frutas in enumerate(frutas):
+    #print(f"{indice} - {frutas}")
+
+
+
+#for i in range(10,30):
+    #if i > 20:
+        #print(f"o primeiro numero maior que 20 é: {i}")
+#break
+
+
+
+
+
